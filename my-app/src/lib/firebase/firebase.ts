f7b06fc5457552f914e746caf0ef278defc8981a
IkuakiR <linyuzhao3@gmail.com>
2025-03-17 17:18:47 +0900

    Firebaseを依存関係に追加し、初期化コードを実装

---
+++ my-app/src/lib/firebase/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD8QIcaxg00Gcn39rf1wISNyidAutyp_7E",
+    authDomain: "crap-game-b3a79.firebaseapp.com",
+    projectId: "crap-game-b3a79",
+    storageBucket: "crap-game-b3a79.firebasestorage.app",
+    messagingSenderId: "610969572681",
+    appId: "1:610969572681:web:228626a37ca8a09b5d7698"
+};
+
+const app = initializeApp(firebaseConfig);
