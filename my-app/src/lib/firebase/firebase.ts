3c3bd34183d70ce3ed323960c4bed823b7964513
IkuakiR <linyuzhao3@gmail.com>
2025-03-17 19:36:03 +0900

    Firebaseの設定を環境変数から取得するように変更し、初期化関数を追加

--- my-app/src/lib/firebase/firebase.ts
+++ my-app/src/lib/firebase/firebase.ts
@@ -1,12 +1,14 @@
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD8QIcaxg00Gcn39rf1wISNyidAutyp_7E",
-    authDomain: "crap-game-b3a79.firebaseapp.com",
-    projectId: "crap-game-b3a79",
-    storageBucket: "crap-game-b3a79.firebasestorage.app",
-    messagingSenderId: "610969572681",
-    appId: "1:610969572681:web:228626a37ca8a09b5d7698"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
+export const initializeFirebaseApp = () =>
+    !getApps().length ? initializeApp(firebaseConfig) : getApp()
